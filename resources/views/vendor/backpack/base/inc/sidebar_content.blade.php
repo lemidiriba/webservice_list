581fb81c8d4b5e2936d1d0cfddeead7c97bc8508
lemidiriba <lemidiriba5@gmail.com>
2023-01-05 12:44:10 +0000

    site template included

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,6 +1,8 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i>
         {{ trans('backpack::base.dashboard') }}</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('web-service') }}'><i class='nav-icon la la-server'></i> Web services</a></li>
+
 
 <!-- Users, Roles, Permissions -->
 <li class="nav-item nav-dropdown">
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Database\Eloquent\Model\WebService;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +16,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $webServices = \App\Models\WebService::all();
+    return view('welcome', compact('webServices'));
 });
